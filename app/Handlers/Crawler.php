1fe288bd7d14fe6da4af0171f953aefc61d7b8cb
vagrant <vagrant@homestead>
2018-08-09 05:41:51 +0000

    add crawler rules

---
+++ app/Handlers/Crawler.php
@@ -0,0 +1,47 @@
+<?
+
+namespace App\Handlers;
+
+require 'QueryList/vendor/autoload.php';
+
+use QL\QueryList;
+
+class Crawler
+{
+    public function DigFeixiaohao($coinCode)
+    {
+       //采集目标
+       $url = 'https://m.feixiaohao.com/currencies/'.$coincode;
+       // 图表api http://mapi.feixiaohao.com/api/coin/coinhisdata/?coinCode=eos&beginTime=1514739661000&&endTime=1514749661000
+       
+       //编写采集规则
+       $rules = array(
+            //币种名称
+            'coinName' => array('.boxTit.coin-tit>h1','text','-small'),
+            // 币种代码
+            'coinCode' => array('.boxTit.coin-tit>h1','text'),
+            // 币价
+            'coinPrice' => array('.price1>.main','text','-i .range-info'),
+            //币价日同比
+            'range' => array('.price1>.main>.range-info>text-red','html'),
+            // 币价对美元
+            'againstUSD' => array('.lowheight>div:eq(0)>span','text'),
+            // 24小时最高价
+            'heigth24H' =>  array('.lowheight>div:eq(1)>span','text'),
+            // 24小时最低价
+            'low24H' => array('.lowheight>div:eq(2)>span','text'),
+            //24小时成交额
+            'volume' => array('.lowheight>div:eq(3)>span','text'),
+            // 24小时换手率
+            'turnoverRate' => array('.lowheight>div:eq(4)>span','text'),
+            //流通市值
+            'marketValue' => array('.box:eq(3)>.mainInfo:eq(1)>.leftside>.val','text'),   
+            //流通量
+            'turnover' => array('.mainInfo2>.leftside>.val:eq(0)','text'),
+            //发行量
+            'distribution' => array('.mainInfo2>.leftside>.val:eq(1)','text')
+       );
+    
+
+    }
+}

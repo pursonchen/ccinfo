4a4638d109bc98ca048b35810e81cf462194029c
vagrant <vagrant@homestead>
2018-08-11 02:42:16 +0000

    Add GetEosData Queue Job

---
+++ app/Jobs/GetEosData.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+use App\Handlers\Crawler;
+use App\Handlers\CrawlerChart;
+
+
+class GetEosData implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $coinCode, $beginTime, $endTime;
+
+    /**
+     * Create a new job instance.
+     * $coinCode    String   货币代码eos
+     * $beginTime   String   数据开始时间 时间戳ms
+     * $endTime
+     * @return void
+     */
+    public function __construct($beginTime = '1533661261000', $endTime = '1533661461000')
+    {
+        $this -> coinCode = 'eos';
+        $this -> beginTime = $beginTime;
+        $this -> endTime = $endTime;
+
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //请求EOS价格等基础数据 $c[0]['coinName']
+        $basicInfo = app(Crawler::class)->DigFeixiaohao($this->coinCode);
+        // 请求EOS价格图表
+        $priceChart = app(CrawlerChart::class)->DigFeixiaohaoChart($this->coinCode, $this -> beginTime,$this -> endTime);
+
+        
+    }
+}
